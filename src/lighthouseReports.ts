cfe9b1a87a9ff2cc36c63b8a5be1647e41508e5f
Mykola Grybyk <mykola.grybyk@gmail.com>
2023-06-22 13:44:24 +0200

    <feat>(<reports>): added writeScoresToJson and buildAverageCsv
    MINOR

--- src/lighthouseReports.ts
+++ src/lighthouseReports.ts
@@ -1,6 +1,33 @@
 import path from 'path'
 import fse from 'fs-extra'
 
+interface NodeDetails {
+    lhId: string
+    devtoolsNodePath: string
+    selector: string
+    boudingRect: { [k: string]: number }
+    snippet: string
+    nodeLabel: string
+}
+
+interface RuleExecutionError {
+    name: string
+    message: string
+}
+
+interface AxeRuleResult {
+    id: string
+    impact?: string
+    tags: Array<string>
+    nodes: Array<{
+        target: Array<string>
+        failureSummary?: string
+        node: NodeDetails
+        relatedNodes: NodeDetails[]
+    }>
+    error?: RuleExecutionError
+}
+
 export interface LighthouseResult {
     lhr: {
         categories: {
@@ -8,15 +35,30 @@ export interface LighthouseResult {
                 score: number
             }
         }
+        audits: {
+            [k: string]: {
+                title: string
+            }
+        }
+    }
+    artifacts: {
+        Accessibility: {
+            violations: Array<AxeRuleResult>
+        }
     }
     comparisonError?: string
+}
+
+interface AccessibilityViolations {
+    title: string
+    nodes: number
 }
 
 export const writeCsvResult = async (
     reportDir: string,
     name: string,
     scores: Record<string, number>,
-    thresholds: Record<string, number>,
+    thresholds: Record<string, number> = {},
     swimlanes: Array<string> = []
 ) => {
     let csvData = Object.keys(scores).join(',')
@@ -25,7 +67,7 @@ export const writeCsvResult = async (
     }
     csvData += '\n'
     csvData += Object.values(scores).join(',')
-    if (swimlanes.length > 0) {
+    if (swimlanes.length > 0 && Object.keys(thresholds).length > 0) {
         csvData += ',' + swimlanes.map((swimlane) => thresholds[swimlane])
     }
     await fse.writeFile(path.join(reportDir, `${name}.csv`), csvData)
@@ -79,3 +121,47 @@ export const getScores = (result: LighthouseResult) =>
         prev[key] = Math.floor(c.score * 100)
         return prev
     }, {} as Record<string, number>)
+
+export const writeScoresToJson = async (lhScoresDir: string, name: string, scores: Record<string, number>, result: LighthouseResult) => {
+    const json = Object.entries(scores).reduce((prev, [k, score]) => {
+        prev[k] = { score }
+        return prev
+    }, {} as Record<string, { score: number; issues?: AccessibilityViolations[] }>)
+    const accessibilityViolations: AccessibilityViolations[] = result.artifacts.Accessibility.violations.map((v) => {
+        return {
+            title: result.lhr.audits[v.id].title,
+            nodes: v.nodes.length,
+        }
+    })
+    if (accessibilityViolations.length > 0) {
+        json.accessibility.issues = accessibilityViolations
+    }
+
+    await fse.writeFile(path.join(lhScoresDir, `${name}.json`), JSON.stringify(json, null, 2))
+}
+
+export const buildAverageCsv = async (lhScoresDir: string) => {
+    const files = await fse.readdir(lhScoresDir)
+    const jsonFiles = files.filter((f) => f.endsWith('.json'))
+
+    // sum all the scores
+    const scores: Record<string, number> = {}
+    for (const fileName of jsonFiles) {
+        const score: Record<string, { score: number; issues?: AccessibilityViolations[] }> = await fse.readJson(
+            path.join(lhScoresDir, fileName)
+        )
+        Object.entries(score).forEach(([k, v]) => {
+            if (!scores[k]) {
+                scores[k] = 0
+            }
+            scores[k] += v.score
+        })
+    }
+
+    // calculate average score
+    Object.entries(scores).forEach(([k, v]) => {
+        scores[k] = v / jsonFiles.length
+    })
+
+    await writeCsvResult(lhScoresDir, '_AVERAGE_', scores)
+}
